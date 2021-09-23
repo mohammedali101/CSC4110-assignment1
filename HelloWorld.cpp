490cd83927c8fd6ff69a5d15f84acc59c9ea4e4d
Mohammed Ali <mohammedali24680@gmail.com>
2021-09-23 16:35:35 -0400

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,4 +1,5 @@
-// Simple C++ program to display "Hello World" 
+// Simple C++ program to display "Hello World"
+// Author: Mohammed Ali <gx0340@wayne.edu>
 // Header file for input output functions 
 #include<iostream>  
   
